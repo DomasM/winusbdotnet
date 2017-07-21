43a6f694018f881d30de497403d9e1ab572c0153
Domas Markevicius <domas.mark@gmail.com>
2017-07-21 14:57:37 +0300

    Remove boilerplate

--- winusbdotnet/NativeMethods.cs
+++ winusbdotnet/NativeMethods.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Win32.SafeHandles;
 
 namespace winusbdotnet {
 
